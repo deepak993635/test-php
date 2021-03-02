9dcf05137a672264831a2c414d7625152a018673
deepak993635 <36846623+deepak993635@users.noreply.github.com>
2021-03-02 17:05:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,27 @@
 <?php
-session_start();
-	if(!isset($_SESSION['browser'])) {
-	$_SESSION['browser'] ="Brave";
-	}
-if(!isset($_SESSION['site'])) {
-	$_SESSION['site'] ="";
-	}
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
 
-include ('functions.php');	
+echo "<br>";
+echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";
+echo $_SERVER['REMOTE_HOST'];
+echo "<br>";
+echo $_SERVER['REMOTE_PORT'];
+echo "<br>";
+echo $_SERVER['PATH_TRANSLATED'];
+echo "<br>";
+echo $_SERVER['SCRIPT_URI'];
 
-$keyword= getkeyword();
-
- if ( isset($_POST['submit']) ) {
-	 
 
 
-$intance = $_POST["intance"];
-$site = $_POST["site"];
-$it = $intance + 1;
-
-$_SESSION['site'] = $site;
-
-$launchcmd='chrome.exe --profile-directory="Profile '.$intance.'" "https://presearch.org/extsearch?term='.$keyword.'"';
-//	echo $launchcmd;
-	exec ($launchcmd);
-
-$_SESSION['intance'] = $it;
-
- header("Location: r.php");
-	
-	
-	
-	}
 ?>
-
-<html>
-<title>PHP Dev Tool </title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="w3.css">
-<body>
-<div class="w3-row-padding">
-<form method="post" action="">
-<div class="w3-third">
-<p>Website</p>
-<select class="w3-select" name="intance" >
-<option><?php   echo  $_SESSION['intance'] ; ?></option>
-<?php  
-$x = 0; 
-while($x <= 750) {
-  echo "<option value=$x> $x </option>";
-  $x++;
-} 
-?></select>
-</div>
-</div>
-
-<button class="w3-btn w3-blue" name="submit" type ="submit">Open</button>
-
-
-</form>
-
-
-<?php
-print_r($_SESSION);
-?><br><br>
-<a href="update-d.php" class="w3-button w3-black" >update</a>
